4d5a7de8e449ac7237c44a9a5c537bce089eda1c
Muhammad Saim Naveed <msaimnaveed2005@gmail.com>
2026-03-25 16:58:58 +0500

    Complete PlayRound Function

--- logic.js
+++ logic.js
@@ -42,39 +42,42 @@ function playGame()
 {
   function PlayRound(humanChoice, computerChoice)
 {
+      console.log("Human chose:", humanChoice);
+      console.log("Computer chose:", computerChoice);
+
     if(humanChoice == computerChoice)
     {
         console.log("TIE!");
     }
     else if(humanChoice == "ROCK" && computerChoice == "SCISSORS")
     {
-        console.log("ROCK beats SCISSORS. HUMAN WINS!");
+        console.log("ROCK beats SCISSORS.");
         humanScore++;
        
     }
     else if(humanChoice == "ROCK" && computerChoice == "PAPER")
     {
-        console.log("PAPER beats ROCK. COMPUTER WINS!");
+        console.log("PAPER beats ROCK.");
          computerScore++;
     }
     else if(humanChoice == "PAPER" && computerChoice == "ROCK")
     {
-        console.log("PAPER beats ROCK. HUMAN WINS!");
+        console.log("PAPER beats ROCK.");
         humanScore++;
     }
     else if(humanChoice == "PAPER" && computerChoice == "SCISSORS")
     {
-        console.log("SCISSORS beats PAPER. COMPUTER WINS!");
+        console.log("SCISSORS beats PAPER.");
          computerScore++;
     }
     else if(humanChoice == "SCISSORS" && computerChoice == "PAPER")
     {
-        console.log("SCISSORS beats PAPER. HUMAN WINS!");
+        console.log("SCISSORS beats PAPER.");
         humanScore++;
     }
     else if(humanChoice == "SCISSORS" && computerChoice == "ROCK")
     {
-        console.log("ROCK beats SCISSORS. COMPUTER WINS!");
+        console.log("ROCK beats SCISSORS.");
          computerScore++;
     }
     else
@@ -83,7 +86,29 @@ function playGame()
     }
 }
   let humanScore=0,computerScore=0;
+  let humanChoice;
+  let computerChoice;
+  for(let i=0;i<5;i++)
+  {
+    humanChoice=getHumanChoice();
+    computerChoice=getComputerChoice();
+    PlayRound(humanChoice,computerChoice);
+  }
+  console.log("----- FINAL SCORE -----");
+  console.log("HUMAN SCORE:",humanScore);
+  console.log("COMPUTER SCORE:",computerScore);
+   if(humanScore > computerScore)
+    {
+        console.log("HUMAN WINS THE GAME!");
+    }
+    else if(computerScore > humanScore)
+    {
+        console.log("COMPUTER WINS THE GAME!");
+    }
+    else
+    {
+        console.log("GAME IS A TIE!");
+    }
 }
-const humanChoice=getHumanChoice();
-const computerChoice=getComputerChoice();
-PlayRound(humanChoice,computerChoice);
+ playGame();
+
